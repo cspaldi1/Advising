d2dee6836bdd728b157d08109bce091ff7b460b7
William Gillespie <wgilles1@emich.edu>
2016-10-30 21:20:35 -0400

    Files to create the tables for the mock database and initialize a dummy database.

---
+++ Database_Utilities/InitializeDummyDB.sql
@@ -0,0 +1,22 @@
+/* populate STUDENT table*/
+INSERT INTO STUDENT (EID, firstName, lastName, email)
+VALUES ('11111111', 'William', 'Gillespie', 'wgilles2');
+
+INSERT INTO STUDENT (EID, firstName, lastName, email)
+VALUES ('11111112', 'Christina', 'Spalding', 'cspalding4');
+
+INSERT INTO STUDENT (EID, firstName, lastName, email)
+VALUES ('11111113', 'Cameron', 'Copland', 'ccopland3');
+
+INSERT INTO STUDENT (EID, firstName, lastName, email)
+VALUES ('11111114', 'John', 'Davis', 'jdavis10');
+
+/* populate ADVISOR table */
+INSERT INTO ADVISOR (advisorID, firstName, lastName, isAdmin, hashedPassword)
+VALUES ('knara', 'Krish', 'Narayanan', 0,'1234');
+
+INSERT INTO ADVISOR (advisorID, firstName, lastName, isAdmin, hashedPassword)
+VALUES ('wsverdlik', 'William', 'Sverdlik', 0, '12345');
+
+INSERT INTO ADVISOR (advisorID, firstName, lastName, isAdmin, hashedPassword)
+VALUES ('bross', 'Bob', 'Ross', 1, '1234');
