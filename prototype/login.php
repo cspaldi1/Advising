3166d7b3649980b50a25fe82ceac5abcd22195cf
ccopland94 <ccopland@live.com>
2016-11-13 12:04:50 -0500

    login

--- prototype/login.php
+++ prototype/login.php
@@ -1,29 +1,37 @@
 <?php
-$servername = "localhost";
-$username = "201609_481_g06";
-$password = "HKECNHGTYHQKJZFGHWCLD";
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, "201609_481_g06");
-
-// Check connection
-if (mysqli_connect_errno()) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-echo "Connected successfully\n";
-
-$query = "SELECT hashedPassword
-          FROM ADVISOR
-          WHERE advisorNetID = 'admin'";
-
-$result = mysqli_query($conn, $query);
-$row=mysqli_fetch_assoc($result);
-var_dump($row);
-if(false)
+if($_POST['netID'] && $_POST['password'])
 {
-  echo "TRUE";
+  $servername = "localhost";
+  $username = "201609_481_g06";
+  $password = "HKECNHGTYHQKJZFGHWCLD";
+
+  // Create connection
+  $conn = mysqli_connect($servername, $username, $password, "201609_481_g06");
+
+  // Check connection
+  if (mysqli_connect_errno()) {
+      die("Connection failed: " . mysqli_connect_error());
+  }
+
+  $query = "SELECT hashedPassword
+            FROM ADVISOR
+            WHERE advisorNetID = 'admin'";
+
+  $result = mysqli_query($conn, $query);
+  $row=mysqli_fetch_assoc($result);
+
+  if(password_verify($_POST['password'], $row['hashedPassword']))
+  {
+    header("Location: home.html");
+    die();
+  } else {
+    echo "FALSE";
+  }
+} else if($_POST['netID'] || $_POST['password']){
+
 } else {
-  echo "FALSE";
+  
 }
 ?>
 
@@ -36,17 +44,19 @@ if(false)
       <div id="header"><span id="title">Honors Advising Portal</span>
       </div>
     </div>
-    <div id="login">
-      <div style="text-align: center;">
-        <span><b>Login</b></span>
+    <form action="login.php" method="post">
+      <div id="login">
+        <div style="text-align: center;">
+          <span><b>Login</b></span>
+        </div>
+        <p>NetID</p>
+        <input type="text" name="netID"/>
+        <p>Password</p>
+        <input type="text" name="password"/><br/>
+        <div style="text-align: center; padding-top: 5px;">
+          <input type="Submit" value="Submit">Login</button>
+        </div>
       </div>
-      <p>NetID</p>
-      <input type="text"/>
-      <p>Password</p>
-      <input type="text"/><br/>
-      <div style="text-align: center; padding-top: 5px;">
-        <button onclick="window.location.href='home.html'">Login</button>
-      </div>
-    </div>
+    </form>
   </body>
 </html>
