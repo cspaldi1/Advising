f96f9c44b85efb1e6bd9078ea4bbd3694d335018
William Gillespie <wgilles1@emich.edu>
2016-11-13 15:25:43 -0500

    Changed email field to studentNetID.

--- Database_Utilities/InitializeDummyDB.sql
+++ Database_Utilities/InitializeDummyDB.sql
@@ -1,14 +1,14 @@
 /* populate STUDENT table*/
-INSERT INTO STUDENT (EID, firstName, lastName, email)
+INSERT INTO STUDENT (EID, firstName, lastName, studentNetID)
 VALUES ('11111111', 'William', 'Gillespie', 'wgilles2');
 
-INSERT INTO STUDENT (EID, firstName, lastName, email)
+INSERT INTO STUDENT (EID, firstName, lastName, studentNetID)
 VALUES ('11111112', 'Christina', 'Spalding', 'cspalding4');
 
-INSERT INTO STUDENT (EID, firstName, lastName, email)
+INSERT INTO STUDENT (EID, firstName, lastName, studentNetID)
 VALUES ('11111113', 'Cameron', 'Copland', 'ccopland3');
 
-INSERT INTO STUDENT (EID, firstName, lastName, email)
+INSERT INTO STUDENT (EID, firstName, lastName, studentNetID)
 VALUES ('11111114', 'John', 'Davis', 'jdavis10');
 
 /* populate ADVISOR table */
