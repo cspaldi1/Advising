4dff13a7c215729fbb7884400cc12a377719df1d
ccopland94 <ccopland@live.com>
2016-11-13 11:35:37 -0500

    password hash test

--- prototype/login.php
+++ prototype/login.php
@@ -10,7 +10,8 @@ $conn = new mysqli($servername, $username, $password);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+echo "Connected successfully\n";
+echo password_hash("password", PASSWORD_DEFAULT)."\n";
 ?>
 
 <html>
