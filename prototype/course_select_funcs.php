abd440df71decb9f252459a8b85ab14838797752
Cameron Copland <ccopland@live.com>
2016-12-04 12:05:32 -0500

    Another one

--- prototype/course_select_funcs.php
+++ prototype/course_select_funcs.php
@@ -212,7 +212,7 @@ if(isset($_POST['action']) && $_POST['action'] != "")
         {
           $times[] = $row['timeStart']." - ".$row['timeEnd'];
         }
-        $result_arr = array("crns"=>$crns, "times"=>$times);
+        $result_arr = array("days"=>$crns, "times"=>$times);
 
         echo json_encode($result_arr);
         break;
