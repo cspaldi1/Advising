ab61c5f705e5f05300a785dc0973b3d9c586bc9d
Cameron Copland <ccopland@live.com>
2016-12-09 00:29:32 -0500

    added foundation call to initialize plugins

--- prototype/home.php
+++ prototype/home.php
@@ -24,7 +24,7 @@
     <nav class="top-bar" data-topbar role="navigation">
       <ul class="title-area">
         <li class="name">
-          <h1><a href="#">Honors Advising Portal</a></h1>
+          <h1><a href="home.php">Honors Advising Portal</a></h1>
         </li>
          <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
         <li class="toggle-topbar menu-icon"><a href="#"><span></span></a></li>
@@ -82,4 +82,7 @@
 
 	 </div>
   </body>
+  <script>
+    $(document).foundation();
+  </script>
 </html>
