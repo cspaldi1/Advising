1e8fc633eb16de0fb96d21ffd4b57e8c15167ff6
Cameron Copland <ccopland@live.com>
2016-12-09 01:00:31 -0500

    added header programmatically

---
+++ prototype/header.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+  <script src="../foundation-6/js/vendor/jquery.js"></script>
+  <script src="../foundation-6/js/vendor/foundation.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="./CSS/foundation.css">
+  <link rel="stylesheet" type="text/css" href="./CSS/foundation.min.css">
+  <link rel="stylesheet" type="text/css" href="./CSS/global.css">
+</head>
+  <body>
+    <nav class="top-bar" data-topbar role="navigation">
+      <ul class="title-area">
+        <li class="name">
+          <h1><a href="home.php">Honors Advising Portal</a></h1>
+        </li>
+         <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
+        <li class="toggle-topbar menu-icon"><a href="#"><span></span></a></li>
+      </ul>
+
+      <section class="top-bar-section">
+        <!-- Left Nav Section -->
+        <ul class="left">
+          <li class="header-menu-item"><a href="student-info.php">Advising</a></li>
+          <li class="header-menu-item"><a href="course-overview.php">Course Info</a></li>
+          <li class="header-menu-item"><a href="users.php">Users</a></li>
+          <!--<li class="has-dropdown">
+            <a href="#">Right Button Dropdown</a>
+            <ul class="dropdown">
+              <li><a href="#">First link in dropdown</a></li>
+              <li class="active"><a href="#">Active link in dropdown</a></li>
+            </ul>
+          </li> -->
+        </ul>
+
+        <!-- Right Nav Section -->
+        <!--<ul class="right">
+          <li><a href="#">User Info</a></li>
+        </ul>-->
+      </section>
+    </nav>
