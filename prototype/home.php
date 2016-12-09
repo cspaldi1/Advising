8625957074ff2b7dbf96776bb21e13358a2ca590
Cameron Copland <ccopland@live.com>
2016-12-09 00:02:57 -0500

    Testing top nav

--- prototype/home.php
+++ prototype/home.php
@@ -19,6 +19,34 @@
   <link rel="stylesheet" type="text/css" href="./CSS/global.css">
 </head>
   <body>
+    <nav class="top-bar" data-topbar role="navigation">
+      <ul class="title-area">
+        <li class="name">
+          <h1><a href="#">My Site</a></h1>
+        </li>
+         <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
+        <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+      </ul>
+
+      <section class="top-bar-section">
+        <!-- Right Nav Section -->
+        <ul class="right">
+          <li class="active"><a href="#">Right Button Active</a></li>
+          <li class="has-dropdown">
+            <a href="#">Right Button Dropdown</a>
+            <ul class="dropdown">
+              <li><a href="#">First link in dropdown</a></li>
+              <li class="active"><a href="#">Active link in dropdown</a></li>
+            </ul>
+          </li>
+        </ul>
+
+        <!-- Left Nav Section -->
+        <ul class="left">
+          <li><a href="#">Left Nav Button</a></li>
+        </ul>
+      </section>
+    </nav>
     <div id="container" class="row">
       <div id="header" class="large-4 columns"><span id="title">Honors Advising Portal</span></div>
 	  <div class="large-4 right columns"><h4>Hello, <?=$_SESSION['user']['fname']?></h4></div>
@@ -50,7 +78,7 @@
 		</div>
 	 </div>
 	 <div id="container" class="row">
-		
+
 	 </div>
   </body>
 </html>
